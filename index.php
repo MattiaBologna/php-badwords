696d4d416754e57fb0fa5e32119455dff5282e8e
MattiaBologna <mattiabologna01@gmail.com>
2024-05-03 15:52:46 +0200

    Add

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Words PHP</title>
+</head>
+
+<body>
+    
+<main>
+    <form action="./server.php" method="GET">
+        <p>
+            <label for="paragraph">Paragrafo</label>
+            <input type="text" name="paragraph" placeholder="Testo di esempio">
+        </p>
+        <p>
+            <label for="curse">Parolaccia</label>
+            <input type="text" name="curse" placeholder="F**K">
+        </p>
+    </form>
+</main>
+    
+</body>
+</html>
